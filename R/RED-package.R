040817b7ed321851fb0bddcb17cbaefd36c7cb26
Daniel E Ehrlich <ehrli097@umn.edu>
2024-09-04 09:49:59 -0500

    changed package documentation to new standard

---
+++ R/RED-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+
+#' @description Package of functions for calculating and visualizing Robust Estimator of grade Difference (RED). RED is a nonparametric method that avoids data compression and allows missing data. Please send any comments, or questions to robustestimator@gmail.com
+#'
+#' @references (2018) Daniels, J., McKean, J., Willermet, C., Edgar, H., Robust Estimator of Grade Differences: a new statistical solution to an old categorical data problem, Chapter 4, Cambridge University Press.
+#'
+#' @references (2016) Willermet, C., Daniels, J., Edgar, H., Seeing RED: A new statistical solution to an old categorical data problem. https://www.researchgate.net/publication/301777157_Seeing_RED_A_new_statistical_solution_to_an_old_categorical_data_problem
+
+"_PACKAGE"
+
+## usethis namespace: start
+
+#' @import rgl
+#' @import stats
+#' @import utils
+#' @import graphics
+
+## usethis namespace: end
+
+
+NULL
